d2f83106ae9593583ce286bd1b95366cc0e46f81
JoGarcia719 <garcia.josephandrew@auf.edu.ph>
2023-05-12 12:24:40 +0800

    Activity 4

---
+++ PDC03_MODULE04/PDC03_MODULE04/activity05.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity05 : ContentPage
+    {
+        List<Contacts> contacts;
+        public activity05()
+        {
+            InitializeComponent();
+
+            SetupData();
+            listView.ItemsSource = contacts;
+            
+         }
+
+        async void onItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        void SetupData()
+        {
+            contacts = new List<Contacts>();
+            contacts.Add(new Contacts
+            {
+                Name = "Juan Dela Cruz",
+                Age = 30,
+                Occupation = "Analyst",
+                Country = "Philippines"
+            });
+            contacts.Add(new Contacts
+            {
+                Name = "Jane Doe",
+                Age = 25,
+                Occupation = "UI/UX",
+                Country = "Philippines"
+            });
+        }
+    }
+}
